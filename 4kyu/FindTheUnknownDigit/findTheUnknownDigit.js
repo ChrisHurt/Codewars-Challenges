e8ec6c9cc1aeca1ab081fbebddaee95948580d61
Chris Hurt <chrishcoding@gmail.com>
2019-10-28 21:02:15 +1100

    Refactored partial solution to unknown digit kata

--- 4kyu/FindTheUnknownDigit/findTheUnknownDigit.js
+++ 4kyu/FindTheUnknownDigit/findTheUnknownDigit.js
@@ -2,31 +2,44 @@ function solveExpression(exp) {
   console.log('original expression: ' + exp);
     let LHSValue, RHSValue;
     let substituteString;
-    for(let i = (exp[0] == '?') ? 1 : 0; i <= 9; i++){
+    let startPos = 0;
+    if( ( (exp[0] == '?') && (exp[2] == '*' || exp[2] == '+' || exp[2] == '-') ) || exp.includes('??') ){startPos = 1;}
+  //   if(exp[0] == '?'){startPos = 1;}
     
+    for(let i = startPos; i <= 9; i++){
+      console.log('i: ' + i);
+      if(exp.includes(i.toString())){continue;}
       substituteString = exp;
       while(substituteString.includes('?')){
         substituteString = substituteString.replace('?',i.toString());
       }
       let [LHS, RHS] = substituteString.split('='); 
-  //     console.log(LHS);
-  //     console.log(RHS);
-      
+      let leftOfOperatorString,rightOfOperatorString = '';
+      let operator = '';
       if(LHS.includes('*')){
-        let leftOfOperator =  Number(LHS.slice(  0,LHS.indexOf('*') ));
-        let rightOfOperator = Number(LHS.slice(LHS.indexOf('*') + 1));
-        LHSValue = leftOfOperator * rightOfOperator;
-        console.log(`leftOfOperator: ${leftOfOperator}, rightOfOperator: ${rightOfOperator}`);
+        operator = '*';
       } else if(LHS.includes('+')){
-        let leftOfOperator =  Number(LHS.slice(  0,LHS.indexOf('+') ));
-        let rightOfOperator = Number(LHS.slice(LHS.indexOf('+') + 1));
-        LHSValue = leftOfOperator + rightOfOperator;
-        console.log(`leftOfOperator: ${leftOfOperator}, rightOfOperator: ${rightOfOperator}`);
+        operator = '+';
       } else if(LHS.includes('-')){
-        let leftOfOperator =  Number(LHS.slice(  0,LHS.indexOf('-') ));
-        let rightOfOperator = Number(LHS.slice(LHS.indexOf('-') + 1));    
-        LHSValue = leftOfOperator - rightOfOperator;
-        console.log(`leftOfOperator: ${leftOfOperator}, rightOfOperator: ${rightOfOperator}`);
+        operator = '-';
+      }
+      leftOfOperatorString =  LHS.slice(  0,LHS.indexOf(operator) );
+      rightOfOperatorString = LHS.slice(LHS.indexOf(operator) + 1);    
+      console.log(`leftOfOperator: ${leftOfOperatorString}, rightOfOperator: ${rightOfOperatorString}`);
+     
+      switch(operator){
+        case '*': 
+          LHSValue = Number(leftOfOperatorString) * Number(rightOfOperatorString);
+          break;
+        case '+': 
+          LHSValue = Number(leftOfOperatorString) + Number(rightOfOperatorString);
+          break;
+        case '-': 
+          LHSValue = Number(leftOfOperatorString) - Number(rightOfOperatorString);
+          break;
+        default:
+          LHSValue = Number(LHS);
+          break;
       }
       
       RHSValue = Number(RHS);
