14aa6ed2cc10123cb5da11f1474c9a0ef8fe9c99
Chris Hurt <chrishcoding@gmail.com>
2019-10-30 14:13:48 +1100

    Scoped approach to nesting structure comparison solution

---
+++ 4kyu/Nesting Structure/nestingStructureComparison.js
@@ -0,0 +1,11 @@
+Array.prototype.sameStructureAs = function (other) {
+    // Recursively call the function to access every single value that is present
+      // Valid end points are
+        // any value
+        // empty array
+        // empty object
+    // Record the accesspath as a string for each endpoint as elements of an array
+    // Iterate through the array and attempt to access each endpoint.
+    // If all pass, it has the same structure
+    // If any fail, it does not have the same structure
+};
