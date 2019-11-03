6420ca86c62eaf8e7321394fec1f04a47435b3b9
Chris Hurt <chrishcoding@gmail.com>
2019-11-03 17:24:24 +1100

    Solved kata

--- 4kyu/Most Frequently Used Words In A Text/mostFrequentlyUsedWordsInAText.js
+++ 4kyu/Most Frequently Used Words In A Text/mostFrequentlyUsedWordsInAText.js
@@ -1,8 +1,7 @@
 function topThreeWords(text) {
-if(text===null || text === undefined)return;
+  if(text===null || text === undefined || text.match(/([\'\w])+/g) === null || (text.match(/([\'\w])+/g) && text.match(/([\'])+/g) && text.match(/([\'\w])+/g).length == text.match(/([\'])+/g).length))return [];
   let wordFrequency = {};
-  let wordList  = text.match(/[&\w]+/g);
-  console.log(`wordList: ${wordList}`);
+  let wordList  = text.match(/([\'\w])+/g).map(word=>word.toLowerCase());
   wordList.forEach(word=>{
     if(wordFrequency.hasOwnProperty(word) == true){
       wordFrequency[word]++;
@@ -10,6 +9,34 @@ if(text===null || text === undefined)return;
       wordFrequency[word] = 1;
     }
   });
-  console.log(wordFrequency);
-  
+  wordList = [];
+  wordList = Object.keys(wordFrequency).reduce((newWordList,currentWord)=>{
+    let injectionPosition = -1;
+    let currentWordFrequency = wordFrequency[currentWord];
+    for(let i = 0; i < newWordList.length; i++){
+      if(currentWord === newWordList[i]){
+        break;
+      }
+      if(currentWordFrequency > wordFrequency[newWordList[i]]){
+        newWordList.splice(i,0,currentWord);
+        break;
+      }
+    }
+    if(wordList.length === 0){
+      newWordList.push(currentWord);
+    }
+    return newWordList;
+  },[])
+
+  wordList = wordList.reduce((newWordList,currentWord)=>{
+    if(!newWordList.includes(currentWord)){
+      newWordList.push(currentWord);
+    }
+    if(wordList.length === 0){
+      newWordList.push(currentWord);
+    }
+    return newWordList;
+  },[]);
+
+  return wordList.slice(0,3);
 }
